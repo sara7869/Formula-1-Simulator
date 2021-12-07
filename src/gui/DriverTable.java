35ea14d5fbed70c8b096d63cae67c7c71f1eb1af
sara7869 <86092128+sara7869@users.noreply.github.com>
2021-12-07 09:28:57 +0530

    Add data to driver table

--- src/gui/DriverTable.java
+++ src/gui/DriverTable.java
@@ -1,10 +1,14 @@
 package gui;
 
-import javax.swing.*;
+import main.Formula1ChampionshipManager;
 
-public class DriverTable extends JPanel{
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class DriverTable extends JPanel {
     JFrame frame = new JFrame();
     JLabel label = new JLabel("Driver Table");
+
     DriverTable() {
         String[] columnNames = {"Name",
                 "Team",
@@ -14,21 +18,41 @@ public class DriverTable extends JPanel{
                 "Third Position Count",
                 "Total Points",
                 "Participated Races Count"};
-        Object[][] data = {
-//                {Main.formula1ChampionshipManager.driverArrayList.name},
-                {"John", "Smith", "Manager", 35, 40000},
-                {"Tom", "Bubble", "Developer", 22, 22000},
-                {"Helen", "Hitchcock", "Project Leader", 30, 34000},
-                {"Kate", "Silva", "Receptionist", 20, 18000},
-                {"Susie", "White", "Developer", 25, 25000}
-        };
+
+        Formula1ChampionshipManager qwe = new Formula1ChampionshipManager();
+
+        int noOfRecords = Formula1ChampionshipManager.driverArrayList.size();
+        int noOfFields = 8;
+        Object[][] data = new Object[noOfRecords][noOfFields];
+
+        for (int count = 0; count < Formula1ChampionshipManager.driverArrayList.size(); count++) {
+            var record = new Object[]{
+                    Formula1ChampionshipManager.driverArrayList.get(count).name,
+                    Formula1ChampionshipManager.driverArrayList.get(count).team,
+                    Formula1ChampionshipManager.driverArrayList.get(count).location,
+                    Formula1ChampionshipManager.driverArrayList.get(count).firstPositionCount,
+                    Formula1ChampionshipManager.driverArrayList.get(count).secondPositionCount,
+                    Formula1ChampionshipManager.driverArrayList.get(count).thirdPositionCount,
+                    Formula1ChampionshipManager.driverArrayList.get(count).totalPoints,
+                    Formula1ChampionshipManager.driverArrayList.get(count).totalPoints};
+            data[count] = record;
+        }
         JTable table = new JTable(data, columnNames);
         table.setFillsViewportHeight(true);
-        //Create the scroll pane and add the table to it.
+        // Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
-        //Add the scroll pane to this panel.
+        // Add the scroll pane to this panel.
         add(scrollPane);
-
+//        TableColumnModel columnModel = table.getColumnModel();
+//        columnModel.getColumn(0).setPreferredWidth(100);
+//        columnModel.getColumn(1).setPreferredWidth(200);
+//        columnModel.getColumn(2).setPreferredWidth(100);
+//        columnModel.getColumn(3).setPreferredWidth(300);
+//        columnModel.getColumn(4).setPreferredWidth(600);
+//        columnModel.getColumn(5).setPreferredWidth(600);
+//        columnModel.getColumn(6).setPreferredWidth(600);
+//        columnModel.getColumn(7).setPreferredWidth(300);
+//        table.setSize(2000,400);
     }
 
 }
