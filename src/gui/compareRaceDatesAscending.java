67404d3f468b0c0703cd72e6aa0ccfa8c0ecb416
sara7869 <86092128+sara7869@users.noreply.github.com>
2021-12-13 18:07:39 +0530

    Sort races comparator written

---
+++ src/gui/compareRaceDatesAscending.java
@@ -0,0 +1,12 @@
+package gui;
+
+import models.Formula1Driver;
+import models.Race;
+
+import java.util.Comparator;
+
+public class compareRaceDatesAscending implements Comparator<Race> {
+    @Override
+    public int compare(Race o1, Race o2) {
+        return o1.date.compareTo(o2.date);    }
+}
